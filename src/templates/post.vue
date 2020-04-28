53c5b1be09a3bdfc8f03f258e27dc960979a753b
iamhmza <shaham902@gmail.com>
2020-04-28 23:07:08 +0000

    styling articles

--- src/templates/post.vue
+++ src/templates/post.vue
@@ -20,8 +20,59 @@ query($id: ID!){
 }
 </page-query>
 
-<script>
-export default {};
-</script>
+<style>
+article {
+  padding: 2rem 0;
+}
+article * + * {
+  margin-top: 1rem;
+}
+/* Links
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+article a {
+  color: #1eaedb;
+}
+article a:hover {
+  color: #0fa0ce;
+}
+/* Lists
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+article ul {
+  list-style: circle inside;
+}
+article ol {
+  list-style: decimal inside;
+}
+article ol,
+article ul {
+  padding-left: 0;
+  margin-top: 0;
+}
+article ul ul,
+article ul ol,
+article ol ol,
+article ol ul {
+  margin: 1.5rem 0 1.5rem 3rem;
+  font-size: 90%;
+}
+article li {
+  margin-bottom: 1rem;
+}
 
-<style></style>
+/* Code
+–––––––––––––––––––––––––––––––––––––––––––––––––– */
+article code {
+  padding: 0.2rem 0.5rem;
+  margin: 0 0.2rem;
+  font-size: 90%;
+  white-space: nowrap;
+  background: #f1f1f1;
+  border: 1px solid #e1e1e1;
+  border-radius: 4px;
+}
+article pre > code {
+  display: block;
+  padding: 1rem 1.5rem;
+  white-space: pre;
+}
+</style>
